34fbb1da8557c9a759aa57717692258faee1a147
geonhoon <geonhoon0403@naver.com>
2022-09-30 17:09:11 +0900

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,78 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__data, .login-wrapper, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+
+
+
+
+const birthYearEl = document.querySelector('#birth-year')
+// option 목록 생성 여부 확인
+isYearOptionExisted = false;
+birthYearEl.addEventListener('focus', function () {
+  // year 목록 생성되지 않았을 때 (최초 클릭 시)
+  if(!isYearOptionExisted) {
+    isYearOptionExisted = true
+    for(var i = 1940; i <= 2022; i++) {
+      // option element 생성
+      const YearOption = document.createElement('option')
+      YearOption.setAttribute('value', i)
+      YearOption.innerText = i
+      // birthYearEl의 자식 요소로 추가
+      this.appendChild(YearOption);
+    }
+  }
+});
+
+
+
+//include html
+
+fetch("./header.html")
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.querySelector("header").innerHTML = data;
+  });
+
+  fetch("./nav.html")
+  .then((response) => {
+      return response.text();
+  })
+  .then((data) => {
+      document.querySelector("nav").innerHTML = data;
+  });
+
+fetch("./footer.html")
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.querySelector("footer").innerHTML = data;
+  });
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+  // board 페이지 넘기기
+  
+  $(document).on('click', '.page-box .btn.number', function(){
+    $('.page-box .btn.number').removeClass('on');
+    $(this).addClass('on');
+    return false;
+  })
